12119d9353d7411df34886cc920c182c836091ef
Aleksandra Lipert <ola.lipert@gmail.com>
2022-12-19 13:34:51 +0100

    Added authors entity, updated db

---
+++ Intive.Core/Entities/Author.cs
@@ -0,0 +1,26 @@
+﻿using Intive.Core.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intive.Core.Entities
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+    }
+}
