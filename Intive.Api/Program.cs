fada82b3aebd11e5536f992ed24c9bdfd13a4ddd
Aleksandra Lipert <ola.lipert@gmail.com>
2022-12-19 10:58:37 +0100

    Added EF Core and DbContext

--- Intive.Api/Program.cs
+++ Intive.Api/Program.cs
@@ -1,8 +1,12 @@
+using Intive.Core.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
