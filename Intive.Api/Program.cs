59cfa120b802948d30886c361c862d32013a080a
Aleksandra Lipert <ola.lipert@gmail.com>
2022-12-19 13:13:15 +0100

    Added books entity, did migration to db

--- Intive.Api/Program.cs
+++ Intive.Api/Program.cs
@@ -1,11 +1,13 @@
 using Intive.Core.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContextConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
