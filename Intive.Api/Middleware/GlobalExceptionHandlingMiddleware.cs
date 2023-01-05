5deafb9bb34461d544a732150ff8f2a13f8f6b0a
Aleksandra Lipert <ola.lipert@gmail.com>
2023-01-05 14:22:29 +0100

    Added global exception handler.

---
+++ Intive.Api/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Intive.Api.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException) throw;
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new { error = "An internal server error has occured" });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
